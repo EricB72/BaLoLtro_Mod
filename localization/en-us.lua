b2c919e093682179caabe11595f37aaaa5c61e9c
EricB72 <eric.baques@estudiant.enti.cat>
2026-06-08 01:05:50 +0200

    Theres a BUG ahhh

---
+++ localization/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        Joker = {
+            j_tstmod_joker_test = {
+                name = 'TestJoker',
+                text = {
+                    '{C:chips}+#1#{} chips'
+                }
+            }
+        }
+    }
+}
--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
+--Atlasses (Sprites)
+SMODS.Atlas {
+    key = 'placeholder',
+    path = 'placeholder.png',
+    px = 71,
+    py = 95
+}
+
+--File Loading
 local jokers_src = SMODS.NFS.getDirectoryItems(SMODS.current_mod.path .. "src/jokers")
 
 for _, file in ipairs(jokers_src) do
-    assert(SMODS.load_file("src/jokers" .. file))()
+    assert(SMODS.load_file("src/jokers/" .. file))()
 end
